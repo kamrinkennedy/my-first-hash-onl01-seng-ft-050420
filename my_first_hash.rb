a284b5d746852be422fd09a2f9bac2ee6a2c76e5
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-07 14:32:18 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,10 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+my_hash = {
+  "name" => "Kamrin",
+  "age" => 29,
+  "height" => "6'1"
+}
   
 end
 
